Add a calculation of original molecule RMSD for easy comparison of training result, fix a bug that when evaluation the dataset is changed in-place

dtmol/diffusion.py
@@ -268,7 +268,7 @@ class BaseSampler(object):
             return self.sample(x)
     
     @staticmethod
-    def _vp_kernel(x_t, score, beta_t, alpha_t, with_noise = True, form = "ddpm"):
+    def _vp_kernel(x_t, score, beta_t, alpha_t, with_noise = True, form = "pc"):
         if form == 'direct':
             x_rev = (np.maximum(np.sqrt(1-beta_t),3))(beta_t*score + x_t)
             #direct form if we reverse the discrete diffusion, note this form would diverge at beta_t = 1
@@ -842,7 +842,7 @@ if __name__ == "__main__":
     tr_sampler.set_T(reverse_T)
     composed.set_T(reverse_T)
     dist_compose, dist_rot, dist_g, dist_tr = [],[],[],[]
-    reverse_with_stochastic = True
+    reverse_with_stochastic = False
     def get_reverse_ts(t,T = 20, old_T = 5000):
         t = t * T // old_T
         return np.arange(t,-1,-1)

dtmol/dtmol_model.py
@@ -1,4 +1,5 @@
 import torch
+import copy
 from torch import nn
 from dtmol.decoder import Decoder
 from dtmol.encoder import UniMolEncoder
@@ -75,6 +76,8 @@ class ScoreNetwork(nn.ModuleDict):
                 "src_edge_type": batch['net_input']['mol_edge_type']}
 
     def eval_once(self,batch,rev_sampler,T = 20, stochastic = False):
+        #copy the batch
+        batch = copy.deepcopy(batch)
         mole_sampler = rev_sampler['molecule']
         prot_sampler = rev_sampler['protein']
         orig_T = mole_sampler.T
@@ -105,7 +108,7 @@ class ScoreNetwork(nn.ModuleDict):
             batch['net_input']['mol_src_distance'] = distance[:,:n_mole,:n_mole]
             batch['net_input']['pocket_distance'] = distance[:,n_mole:,n_mole:]
             batch['net_input']['cross_distance'] = distance[:,:n_mole,n_mole:]
-            ### Dubbing code 
+            ### Debugging code 
             # import time
             # current_time = time.strftime("%Y%m%d_%H%M%S")
             # mole_diff = torch.norm(mol_coord.cpu()-batch['net_input']['mol_src_coord'].cpu())

dtmol/dtmol_train.py
@@ -60,12 +60,12 @@ class DiffusionTrainer(Trainer):
                     self.logger.info(msg)
                 mole_rmsds,prot_rmsds = [],[]
                 for eval_i,eval_batch in enumerate(self.eval_ds):
-                    mole_rmsd,prot_rmsd = self.eval_step(eval_batch)
-                    mole_rmsd_mean,prot_rmsd_mean = mole_rmsd.mean().item(),prot_rmsd.mean().item()
-                    mole_rmsd_std,prot_rmsd_std = mole_rmsd.std().item(),prot_rmsd.std().item()
+                    mole_rmsd,mole_rmsd_baseline,prot_rmsd = self.eval_step(eval_batch)
+                    mole_rmsd_mean, prot_rmsd_mean, mole_rmsd_baseline_mean = mole_rmsd.mean().item(),prot_rmsd.mean().item(),mole_rmsd_baseline.mean().item()
+                    mole_rmsd_std, prot_rmsd_std, mole_rmsd_baseline_std = mole_rmsd.std().item(),prot_rmsd.std().item(), mole_rmsd_baseline.std().item()
                     mole_rmsds += mole_rmsd.tolist()
                     prot_rmsds += prot_rmsd.tolist()
-                    msg = f"Eval {eval_i}/{len(self.eval_ds)}: Mole RMSD {mole_rmsd_mean:.2f} +- {mole_rmsd_std:.2f}, Prot RMSD {prot_rmsd_mean:.2f} +- {prot_rmsd_std:.2f}"
+                    msg = f"Eval {eval_i}/{len(self.eval_ds)}: Mole RMSD {mole_rmsd_mean:.2f} +- {mole_rmsd_std:.2f}, Original Mole RMSD {mole_rmsd_baseline_mean:.2f} +- {mole_rmsd_baseline_std:.2f}, Prot RMSD {prot_rmsd_mean:.2f} +- {prot_rmsd_std:.2f}"
                     self.logger.info(msg)
                 mole_rmsd = np.mean(mole_rmsds)
                 prot_rmsd = np.mean(prot_rmsds)
@@ -181,7 +181,10 @@ class DiffusionTrainer(Trainer):
                                                                       stochastic=self.config.TRAIN['stochastic_reverse_sampling'])
             label = self.get_label_coord(batch)
             mole_rmsd,prot_rmsd = self.rmsd(coord, label, mole_padding, prot_padding)
-        return mole_rmsd, prot_rmsd
+            #calculate the original rmsd
+            orig_coord = torch.cat([batch['net_input']['mol_src_coord'],batch['net_input']['pocket_src_coord']],dim=1)
+            mole_rmsd_ori,prot_rmsd_ori = self.rmsd(orig_coord,label,mole_padding, prot_padding)
+        return mole_rmsd, mole_rmsd_ori, prot_rmsd
     
     def get_label_coord(self, batch):
         label = torch.cat([batch['net_input']['mol_holo_coord'],batch['net_input']['pocket_holo_coord']],dim=1)

dtmol/utils/datasets.py
@@ -146,7 +146,7 @@ class CrossEdgeTypeDataset:
     def __len__(self):
         return len(self.mol_dataset)
 
-    @lru_cache(maxsize=16)
+    @lru_cache(maxsize=1)
     def __getitem__(self, idx):
         source = self.mol_dataset[idx].clone()
         target = self.pocket_dataset[idx].clone()
@@ -172,11 +172,11 @@ class DiffusionDataset(BaseWrapperDataset):
         super().set_epoch(epoch)
         self.epoch = epoch
 
-    @lru_cache(maxsize=16)
     def __cached_item__(self, index: int, epoch: int):
         item = np.array(self.dataset[index])[None,...]
         with data_utils.numpy_seed(self.seed, epoch, index), torch_seed(self.seed, epoch, index):
             diffused,score,norm,time_steps = self.diffuser(item)
+            self.seed = torch_seed(self.seed, epoch, index)
         return {"diffused": diffused[0].to(torch.float32), "score": score[0],"norm":norm[0], "time_steps":time_steps}
     
     def __getitem__(self, index: int):
@@ -191,7 +191,7 @@ class SliceDataset(BaseWrapperDataset):
     def __len__(self):
         return len(self.dataset)
 
-    @lru_cache(maxsize=16)
+    @lru_cache(maxsize=1)
     def __getitem__(self, index):
         item = self.dataset[index]
         if self.start:
@@ -809,7 +809,14 @@ class CrossDataset(DictDataset):
 if __name__ == "__main__":
     from dtmol.utils.dictionary import Dictionary
     from dtmol.dtmol_init import PRETRAIN_FOLDER
+    from dtmol.utils.sampling import rmsd as rmsd_import
     from matplotlib import pyplot as plt
+    def rmsd(mol1, mol2):
+        """
+        mol1: [n,3]
+        mol2: [n,3]
+        """
+        return torch.mean(torch.sqrt(torch.sum((mol1 - mol2)**2,dim = -1)))
     ligand_dict = Dictionary.load(f"{PRETRAIN_FOLDER}/unimol_molecule_dict.txt")
     protein_dict = Dictionary.load(f"{PRETRAIN_FOLDER}/unimol_protein_dict.txt")
     protein_path = "/data/unimol_data/protein_ligand_binding_pose_prediction/"
@@ -823,17 +830,20 @@ if __name__ == "__main__":
     pocket_dataset.transform("train")
 
     #3D plot the pocket and ligand
-    idx = 0
+    idx = 2
     from mpl_toolkits.mplot3d import Axes3D
     fig = plt.figure()
     print(pocket_dataset['train'][idx]['pocket_name'])
     print(pocket_dataset['train'][idx]['smi_name'])
     pocket = pocket_dataset['train'][idx]['net_input.pocket_holo_coord']
     ligand = pocket_dataset['train'][idx]['net_input.mol_holo_coord']
+    ligand_src = pocket_dataset['train'][idx]['net_input.mol_src_coord']
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(pocket[:, 0], pocket[:, 1], pocket[:, 2], c='pink', marker='o', label = "pocket")
+    # ax.scatter(pocket[:, 0], pocket[:, 1], pocket[:, 2], c='pink', marker='o', label = "pocket")
     ax.scatter(ligand[:, 0], ligand[:, 1], ligand[:, 2], c='b', marker='o', label = "ligand")
-    
+    ax.scatter(ligand_src[:, 0], ligand_src[:, 1], ligand_src[:, 2], c='purple', marker='o', label = "ligand rdkit")
+    print("RMSD of ligand and ligand rdkit: ", rmsd(ligand_src[1:-1], ligand[1:-1]))
+    print("RMSD2 of ligand and ligand rdkit: ", rmsd_import(ligand_src.unsqueeze(0), ligand.unsqueeze(0), torch.zeros(1,ligand.shape[0]-1).bool(), torch.zeros(1,1,dtype = torch.bool).unsqueeze(0)[:,:,0]))
 
     #Test diffuser
     from dtmol.diffusion import RotationSampler, GaussianSampler, TranslationSampler, ChainSampler
@@ -842,9 +852,9 @@ if __name__ == "__main__":
     cos_sch = CosineScheduler(T)
     geo_sch = GeometricScheduler(T)
     poly_sch = PolynomialScheduler(T)
-    rot_sampler = RotationSampler(schedular=geo_sch)
-    g_sampler = GaussianSampler(schedular = poly_sch)
-    g_sampler2 = GaussianSampler(schedular = geo_sch)
+    rot_sampler = RotationSampler(schedular=cos_sch)
+    g_sampler = GaussianSampler(schedular = cos_sch)
+    g_sampler2 = GaussianSampler(schedular = cos_sch)
     tr_sampler = TranslationSampler(schedular = cos_sch)
     molecule_sampler = ChainSampler(rot_sampler).compose(tr_sampler).compose(g_sampler)
     protein_sampler = ChainSampler(g_sampler2)
@@ -858,8 +868,9 @@ if __name__ == "__main__":
     diffused_pocket = diffuse_dataset['train'][idx]['diffused.pocket_holo_coord']
     diffused_ligand = diffuse_dataset['train'][idx]['diffused.mol_holo_coord']
     assert diffuse_dataset['train'][idx]['diffused.pocket_diffuse_time'] == diffuse_dataset['train'][idx]['diffused.mol_diffuse_time']
-    ax.scatter(diffused_pocket[:, 0], diffused_pocket[:, 1], diffused_pocket[:, 2], c='r', marker='o', label = "diffused pocket")
+    # ax.scatter(diffused_pocket[:, 0], diffused_pocket[:, 1], diffused_pocket[:, 2], c='r', marker='o', label = "diffused pocket")
     ax.scatter(diffused_ligand[:, 0], diffused_ligand[:, 1], diffused_ligand[:, 2], c='g', marker='o', label = "diffused ligand")
+    print("RMSD of ligand and diffused ligand: ", rmsd(diffused_ligand[1:-1], ligand[1:-1]))
     plt.legend()
 
     print(f"Diffuse ligand score shape: {diffuse_dataset['train'][idx]['diffused.mol_diffuse_perturb_score'].shape}")