Fix a bug that cause the label score dimension mismatch the prediction (x,y,z mismatch); allow none reduction in loss output, enable individual inspection of translation loss and rotation loss

dtmol/block.py
@@ -805,7 +805,10 @@ class DiffusionHead(nn.Module):
         y = y.reshape(bsz,n,self.out_dim) # [B, N, O]
         return self.x_gate(x)*y
 
-    def loss(self, output, score, norm, padding_mask = None, norm_weighted = False):
+    def loss(self, output, score, norm, 
+             padding_mask = None, 
+             norm_weighted = False,
+             reduction = "mean"):
         loss = self.mse_loss(output, score)
 
         # ### Debugging code ###
@@ -823,7 +826,15 @@ class DiffusionHead(nn.Module):
         if padding_mask is not None:
             mask = mask * (~padding_mask.unsqueeze(-1))
             loss = loss * (~padding_mask.unsqueeze(-1))
-        return loss[mask.squeeze(-1)].mean()
+        loss = loss[mask.squeeze(-1)]
+        if reduction == "mean":
+            return loss.mean()
+        elif reduction == "none":
+            return loss
+        elif reduction == "sum":
+            return loss.sum()
+        else:
+            raise ValueError("Invalid reduction type")
 
 class DiffusionPoolHead(nn.Module):
     """Head for system-level diffusion noise."""
@@ -863,15 +874,23 @@ class DiffusionPoolHead(nn.Module):
         x = self.out_proj(x)
         y = y.permute(0,2,3,1) # [B, N, 3, H]
         y = self.out_proj2(y) # [B, N, 3, O/3]
+        y = y.permute(0,1,3,2) # [B, N, O/3, 3]
         y = y.reshape(bsz,n,self.out_dim) # [B, N, O]
         out = self.x_gate(x)*y # [B, N, O]
         return out.mean(dim=1)
 
-    def loss(self, output, score, norm, norm_weighted = False):
+    def loss(self, output, score, norm, norm_weighted = False,reduction = "mean"):
         loss = self.mse_loss(output, score)
         if norm_weighted:
             loss = loss / norm.unsqueeze(-1)
-        return loss.mean()
+        if reduction == "mean":
+            return loss.mean()
+        elif reduction == "none":
+            return loss
+        elif reduction == "sum":
+            return loss.sum()
+        else:
+            raise ValueError("Invalid reduction type")
 
 class DiffusionClassificationHead(nn.Module):
     """Head for system-level diffusion noise."""
@@ -912,14 +931,22 @@ class DiffusionClassificationHead(nn.Module):
         x = self.out_proj(x)
         y = y.transpose(1, 2) # [B, 3, H]
         y = self.out_proj2(y) # [B, 3, O/3]
+        y = y.permute(0,2,1) # [B, O/3, 3]
         y = y.reshape(-1,self.out_dim) # [B, O]
         return self.x_gate(x)*y
 
-    def loss(self, output, score, norm, norm_weighted = False):
+    def loss(self, output, score, norm, norm_weighted = False,reduction = "mean"):
         loss = self.mse_loss(output, score)
         if norm_weighted:
             loss = loss / norm.unsqueeze(-1)
-        return loss.mean()
+        if reduction == "mean":
+            return loss.mean()
+        elif reduction == "none":
+            return loss
+        elif reduction == "sum":
+            return loss.sum()
+        else:
+            raise ValueError("Invalid reduction type")
 
 class NonLinearHead(nn.Module):
     """Head for simple classification tasks."""

dtmol/dtmol_model.py
@@ -191,7 +191,8 @@ class ScoreNetwork(nn.ModuleDict):
                        perturbation_diffusion = True, 
                        trrot_diffusion = True, 
                        atom_diffusion=False, 
-                       norm_weighted=False):
+                       norm_weighted=False,
+                       reduction = "mean"):
         if not(perturbation_diffusion) and not(trrot_diffusion) and not(atom_diffusion):
             raise ValueError("No diffusion loss has been enabled.")
         losses = {}
@@ -209,7 +210,8 @@ class ScoreNetwork(nn.ModuleDict):
             trrot_loss = self['decoder'].diffusion_heads['tr-rotation'].loss(tr_rot, 
                                                                             mol_trrot_score, 
                                                                             norm = mol_trrot_norm,
-                                                                            norm_weighted = True)
+                                                                            norm_weighted = True,
+                                                                            reduction = reduction)
             losses['trrot_loss'] = (self.trrot_weight*trrot_loss)
         if perturbation_diffusion:
             padding_mask[:,0] = True # The first token <s> is for the tr-rotation loss
@@ -217,7 +219,8 @@ class ScoreNetwork(nn.ModuleDict):
                                                                             perturbation_score, 
                                                                             norm = perturbation_norm, 
                                                                             padding_mask = padding_mask, 
-                                                                            norm_weighted = norm_weighted)
+                                                                            norm_weighted = norm_weighted,
+                                                                            reduction = reduction)
             losses["perturbation_loss"] = self.pert_weight*pert_loss
         if atom_diffusion:
             raise NotImplementedError("Atom diffusion is not implemented yet.")

dtmol/dtmol_train.py
@@ -139,21 +139,27 @@ class DiffusionTrainer(Trainer):
             if schedular is not None:
                 schedular.step()
 
-    def loss(self, output, padding_mask, batch,norm_weighted = False):
+    def loss(self, output, padding_mask, batch,norm_weighted = False, validation = False):
+        if validation:
+            reduction = "none"
+        else:
+            reduction = "mean"
         if self.distributed:
             losses = self.nets.module.diffusion_loss(output, 
                                                      padding_mask.clone(), 
                                                      batch['diffused'], 
                                                      norm_weighted=norm_weighted,
                                                      trrot_diffusion = self.config.TRAIN['trrot_loss'],
-                                                     perturbation_diffusion = self.config.TRAIN['perturbation_loss'])
+                                                     perturbation_diffusion = self.config.TRAIN['perturbation_loss'],
+                                                     reduction = reduction)
         else:
             losses = self.nets.diffusion_loss(output, 
                                               padding_mask.clone(), 
                                               batch['diffused'],
                                               norm_weighted=norm_weighted,
                                               trrot_diffusion = self.config.TRAIN['trrot_loss'],
-                                              perturbation_diffusion = self.config.TRAIN['perturbation_loss'])
+                                              perturbation_diffusion = self.config.TRAIN['perturbation_loss'],
+                                              reduction = reduction)
         return losses
 
     def rmsd(self, coord, label, mole_padding, prot_padding):
@@ -169,11 +175,17 @@ class DiffusionTrainer(Trainer):
     def valid_step(self, batch):
         with torch.no_grad():
             output, padding_mask = self.nets(batch)
-            losses = self.loss(output, padding_mask, batch,norm_weighted=False)
-            trrot_loss = losses['trrot_loss'] if self.config.TRAIN['trrot_loss'] else None
-            pert_loss = losses['perturbation_loss'] if self.config.TRAIN['perturbation_loss'] else None    
+            losses = self.loss(output, padding_mask, batch,norm_weighted=False, validation = True)
+            trrot_loss = losses['trrot_loss'] if self.config.TRAIN['trrot_loss'] else None #shape [batch_size, 2, 3]
+            pert_loss = losses['perturbation_loss'] if self.config.TRAIN['perturbation_loss'] else None #shape [batch_size, N, 3]
+            print(pert_loss.shape)
+            pert_loss = pert_loss.mean() if pert_loss is not None else None
+            rotation_loss = trrot_loss[:,0,:].mean() if trrot_loss is not None else None
+            translation_loss = trrot_loss[:,1,:].mean() if trrot_loss is not None else None
             if self.use_wandb and self._on_main_rank():
-                wandb.log({"valid_trrot_loss": trrot_loss,"perturbation loss":pert_loss, "global_step": self.global_step})
+                wandb.log({"valid_rotation_loss": rotation_loss,
+                           "valid_translation_loss": translation_loss, 
+                           "perturbation loss":pert_loss, "global_step": self.global_step})
         return trrot_loss, pert_loss
 
     def eval_step(self, batch):